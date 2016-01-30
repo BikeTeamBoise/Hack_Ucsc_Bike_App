435faac886a4d52e77cb501358cfddcde451943e
Trevor Ching <trevor.ching2@gmail.com>
2016-01-30 02:45:24 -0800

    Can now switch between the map and menu thing

--- app/src/main/java/hack16/hackathon/MainActivity.java
+++ app/src/main/java/hack16/hackathon/MainActivity.java
@@ -1,5 +1,6 @@
 package hack16.hackathon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,8 +81,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.maintomap) {
             // Handle the camera action
+            Intent i = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(i);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -98,4 +101,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }

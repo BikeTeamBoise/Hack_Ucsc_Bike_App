793a0f28735ee6b9964f4e76e58b9d6853dc6587
Kyle Batross <kmcraebatross@gmail.com>
2016-01-29 22:04:58 -0800

    Changed Starting Location
    Starting Location moved to Santa Cruz

--- app/src/main/java/hack16/hackathon/MapsActivity.java
+++ app/src/main/java/hack16/hackathon/MapsActivity.java
@@ -41,8 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng santaCruz = new LatLng(36.9719, -122.0264);
+        mMap.addMarker(new MarkerOptions().position(santaCruz).title("Marker in Santa Cruz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(santaCruz));
     }
 }

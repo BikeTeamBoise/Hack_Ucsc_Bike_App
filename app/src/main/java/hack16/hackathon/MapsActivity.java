93d684f9a3d1b06f7c303361e58dd68e2169edb8
Trevor Ching <trevor.ching2@gmail.com>
2016-01-30 19:28:33 -0800

    I think the proper files were added

--- app/src/main/java/hack16/hackathon/MapsActivity.java
+++ app/src/main/java/hack16/hackathon/MapsActivity.java
@@ -3,13 +3,11 @@ package hack16.hackathon;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
-import java.lang.Throwable;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,34 +15,42 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.location.FusedLocationProviderApi;
-import android.location.Geocoder;
-import android.location.Address;
 
-import java.util.*;
-import java.lang.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.util.Log;
 import android.location.Location;
 
-import java.lang.Object;
-import android.location.LocationManager;
 import android.content.IntentSender;
 import android.view.View;
-import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
+
+    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543, -73.998585);
+    private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
+    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
@@ -63,6 +69,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+/*
+        MarkerOptions options = new MarkerOptions();
+        options.position(LOWER_MANHATTAN);
+        options.position(BROOKLYN_BRIDGE);
+        options.position(TIMES_SQUARE);
+        mMap.addMarker(options);*/
+        ArrayList<LatLng> points = new ArrayList<>();
+        points.add(new LatLng(36.977176, -122.053814));
+        points.add(new LatLng(36.975416, -122.052602));
+        points.add(new LatLng(36.974651, -122.058374));
+        points.add(new LatLng(36.970074, -122.058084));
+        String url = getMapsApiDirectionsUrl(points);
+        ReadTask downloadTask = new ReadTask();
+        downloadTask.execute(url);
+
         mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(10 * 1000)        // 10 seconds, in milliseconds
@@ -72,9 +93,115 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-
     }
 
+    private String getMapsApiDirectionsUrl(ArrayList<LatLng> points){
+        String origin = "origin="+points.get(0).latitude+","+points.get(0).longitude;
+        String destination= "destination="+points.get(points.size()-1).latitude+","+points.get(points.size()-1).longitude;
+        String waypoints = "waypoints=";
+        for(int i=1; i<points.size()-2;i++){
+            if(i==points.size()-2){
+                waypoints = waypoints+points.get(i).latitude+","+points.get(i).longitude;
+            }
+            else {
+                waypoints = waypoints + points.get(i).latitude+","+points.get(i).longitude + "|";
+            }
+        }
+
+        /*String waypoints = "waypoints="
+                + LOWER_MANHATTAN.latitude+","+LOWER_MANHATTAN.longitude
+                +"|"+ BROOKLYN_BRIDGE.latitude+","+BROOKLYN_BRIDGE.longitude
+                +"|"+TIMES_SQUARE.latitude+","+TIMES_SQUARE.longitude;
+        String origin = "origin=Brooklyn";
+        String destination = "destination=Queens";*/
+        String mode = "mode=bicycling";
+        String key = "AIzaSyA4sV9WEO9GRF42EsvaO-9WDq9iWFMrpao";
+        String params = origin+"&"+waypoints+"&"+destination+"&"+mode+"&key="+key;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/"
+                +output+"?"+params;
+        return url;
+    }
+
+    /**
+     * private void addMarkers() {
+     if (googleMap != null) {
+     googleMap.addMarker(new MarkerOptions().position(BROOKLYN_BRIDGE)
+     .title("First Point"));
+     googleMap.addMarker(new MarkerOptions().position(LOWER_MANHATTAN)
+     .title("Second Point"));
+     googleMap.addMarker(new MarkerOptions().position(WALL_STREET)
+     .title("Third Point"));
+     }
+     }
+     */
+
+    private class ReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url){
+            String data = "";
+            try{
+                HttpConnection http = new HttpConnection();
+                data = http.readUrl(url[0]);
+            }
+            catch(IOException e){
+                return "Unable to retrieve web page.";
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>>{
+        @Override
+        protected List<List<HashMap<String,String>>> doInBackground(String... jsonData){
+            JSONObject jo;
+            List<List<HashMap<String,String>>> routes = null;
+
+            try{
+                jo = new JSONObject(jsonData[0]);
+                PathJSONParser parser = new PathJSONParser();
+                routes = parser.parse(jo);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        protected void onPostExecute(List<List<HashMap<String,String>>> routes){
+            ArrayList<LatLng> points = null;
+            PolylineOptions polyLineOptions = null;
+
+            // traversing through routes
+            for (int i = 0; i < routes.size(); i++) {
+                points = new ArrayList<LatLng>();
+                polyLineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                polyLineOptions.addAll(points);
+                polyLineOptions.width(5);
+                polyLineOptions.color(Color.BLUE);
+            }
+
+            mMap.addPolyline(polyLineOptions);
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -88,7 +215,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.moveCamera(CameraUpdateFactory.zoomIn());
+
 
         // Add a marker in Santa Cruz and move the camera
        /*
@@ -166,7 +293,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
     /*called if mMap is not null - puts marker down*/
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.00221,-122.055745)).title("Upper Campus Trail"));
+        mMap.addMarker(new MarkerOptions().position(TIMES_SQUARE).title("Times_square"));
+        mMap.addMarker(new MarkerOptions().position(LOWER_MANHATTAN).title("Lower manhattan"));
+        mMap.addMarker(new MarkerOptions().position(BROOKLYN_BRIDGE).title("Brooklyn bridge"));
 
     }
     /*sets up map if possible*/
@@ -179,8 +309,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
+                //addLines();
             }
         }
+    }
+
+    private void addLines(){
+        mMap.addPolyline((new PolylineOptions()).add(TIMES_SQUARE,BROOKLYN_BRIDGE,LOWER_MANHATTAN,
+                TIMES_SQUARE).width(5).color(Color.BLUE).geodesic(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOWER_MANHATTAN,13));
     }
     @Override
     protected void onResume() {
